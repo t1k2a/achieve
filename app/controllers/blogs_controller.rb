bcbd10d8428c53008447bf269b467bcf1de93cab
t1k2a <maxtj1122@gmail.com>
2019-04-20 08:23:47 +0000

    [add] Add CRUD and standardize

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,48 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:edit, :update, :destroy]
+  
   def index
+    @blogs = Blog.all
   end
   
   def new
+    @blog = Blog.new
   end
   
   def create
+    if @blog.save
+      redirect_to blogs_path, notice: "ブログを作成しました！"
+    else
+      render action: 'new'
+    end
+  end
+  
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+  
+  def update
+    @blog = Blog.find(params[:id])
     
+    if @blog.update(blogs_params)
+      redirect_to blogs_path, notice: "ブログを更新しました！"
+    else
+      render action: 'edit'
+    end
+  end
+  
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_path, notice: "ブログを削除しました！"
+  end
+
+  private
+  def blogs_params
+    params.require(:blog).permit(:title, :content)
+  end
+  
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :blogs, only: [:index, :new, :create]
+  resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy] do
+    collection do
+      post :confirm
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
