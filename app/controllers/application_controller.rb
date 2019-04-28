acf954c15505ff70144c1cea752304d62152eaa8
t1k2a <maxtj1122@gmail.com>
2019-04-28 01:37:34 +0000

    [add] Add a Contact function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   
   protected
     def configure_permitted_parameters
-      devise_parameter_santizer.permit(:signed_up, keys: PERMISSIBLE_ATTRIBUTES)
-      devise_parameter_santizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES)
+      devise_parameter_sanitizer.permit(:signed_up, keys: PERMISSIBLE_ATTRIBUTES)
+      devise_parameter_sanitizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES)
   end
 end
