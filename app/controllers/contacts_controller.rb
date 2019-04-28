d675639b3db3d05cfc6e20a915d68d7360268371
t1k2a <maxtj1122@gmail.com>
2019-04-28 01:37:34 +0000

    [add] Add a Contact function

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,12 +5,14 @@ class ContactsController < ApplicationController
     
     def create
         @contact = Contact.new(contacts_params)
+        # @contact = Contact.new(contacts_params)
         if @contact.save
-            render action: 'new'
+            redirect_to new_contact_path, notice: "お問い合わせいただきありがとうございます"
         end
     end
     
     private def contacts_params
         params.require(:contact).permit(:name, :email, :content)
+   
     end
 end
