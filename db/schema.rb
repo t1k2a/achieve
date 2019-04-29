9f82c477826274bdabc387c9efb2e4aa183c6f2f
t1k2a <maxtj1122@gmail.com>
2019-04-29 01:27:45 +0000

    [update] Insert dummy data, add user_id in blog index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190426080014) do
+ActiveRecord::Schema.define(version: 20190429003142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20190426080014) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "contacts", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# 100.times do |n|
+#     email = Faker::Internet.email
+#     password = "password"
+#     User.create!(email: email, password: password, password_confirmation: password,)
+# end
+
+100.times do |n|
+    Blog.create(title: "seederData", content: "テストデータ", user_id: n)
+    n = n + 1
+end
