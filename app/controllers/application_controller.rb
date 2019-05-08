c13b5f75289497625c2e752b854f1f1641df42bf
t1k2a <maxtj1122@gmail.com>
2019-05-08 07:15:06 +0000

    [add] Add exception and Error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
   
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -8,8 +9,12 @@ class ApplicationController < ActionController::Base
   PERMISSIBLE_ATTRIBUTES = %i(name)
   
   protected
+   rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_url, :alert => exception.message
+  end
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:signed_up, keys: PERMISSIBLE_ATTRIBUTES)
       devise_parameter_sanitizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES)
   end
 end
+
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,5 @@ Rails.application.configure do
   
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.deliverly_method = :letter_opener_web
+  BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
 end
