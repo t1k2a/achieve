7c30f449eec0c8fbb5196e156cd1ea9761637bc0
t1k2a <maxtj1122@gmail.com>
2019-05-16 11:34:20 +0000

    [add] Follow function

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :validatable
 
          has_many :blogs, dependent: :destroy
-  # CommentモデルのAssociationを設定
-  has_many :comments, dependent: :destroy
+         has_many :comments, dependent: :destroy
+         
+         has_many :relationships, foreign_key: "followed_id", dependent: :destroy
+         has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+         
+         has_many :followed_users, through: :relationships, source: :follower
+         has_many :folllowers, through: :reverse_relationships, source: :follower
 end
