334647b241d4b790654206fdd7c63b395c6589e4
t1k2a <maxtj1122@gmail.com>
2019-05-18 17:28:58 +0000

    [update] A little modify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  
+  def edit 
+    @users = User.find(:id)
+  end
+  
+  private
+  def users_params
+    params.require(:user).permit(:id)
+  end
 end
